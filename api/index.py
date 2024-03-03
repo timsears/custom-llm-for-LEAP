f278aea730e2c6de252f9c34c9806d613a5b9d2f
Tim Sears <tim@timsears.com>
2024-03-03 10:13:21 -0800

    skip translateion to english, reply in Arabic. add loggoing.

--- api/index.py
+++ api/index.py
@@ -46,27 +46,28 @@ def chat_completions():
        return "chat_completions: Unsupported model: " + model , 400
        #raise Exception ("chat_completions: Unsupported model: ", model)
 
-    print("model: " + model)
+    print("\nmodel: " + model)
 
     messages = data.get('messages', [])
 
     if not messages:
         return jsonify({'error': 'No messages provided'}), 400
 
-    # Translate all user messages to English
+    # DONT TRANSLATE INCOMING
+    print("\nincoming messages:\n")
     for message in messages:
-        if message['role'] == 'user':
-            detected_lang = translate_client.detect_language(message['content'])['language']
-            print("incoming content: " + message['content'])
-            print("deteced_lang: " + detected_lang)
-            if detected_lang != 'en':
-                x = translate_client.translate(
-                    message['content'],
-                    source_language = 'ar',
-                    target_language='en',
-                    model='nmt')['translatedText']
-                message['content'] = x
-                print("translated content: " + x)
+        print("... " + message['content'])
+        # if message['role'] == 'user':
+        #     detected_lang = translate_client.detect_language(message['content'])['language']
+        #     if detected_lang != 'en':
+        #         x = translate_client.translate(
+        #             message['content'],
+        #             source_language = 'ar',
+        #             target_language='en',
+        #             model='nmt')['translatedText']
+        #         message['content'] = x
+        #         print("translated content: " + x)
+
 
     # Call OpenAI's completion API with the translated conversation
     response = client.chat.completions.create(
@@ -74,20 +75,21 @@ def chat_completions():
         messages=messages
     )
 
-    # Assuming response should be translated back to the original language if needed
+    print("\nraw llm response:\n", response)
     completion_text = (response.choices)[0].message.content
-    print("llm response: " + completion_text)
+    print("\nllm response: " + completion_text)
 
     # if detected_lang != 'en':
     #     completion_translated = translate_client.translate(completion_text, target_language='ar', format_='text', model='nmt')['translatedText']
     # else:
     #     completion_translated = completion_text
 
+    # DO Translate response to Arabic
     target_language = 'ar'
     completion_translated = translate_client.translate(completion_text, target_language='ar', format_='text', model='nmt')['translatedText']
+    print("\n: translated response: ", completion_translated)
 
     # Construct and return the response object
-
     llm_response = {
         "id": response.id,
         "object": "chat.completion",
@@ -102,7 +104,7 @@ def chat_completions():
         }]
     }
 
-    print (llm_response)
+    print ("\n",llm_response)
 
     # Convert message into data packet.
     def generate(message):
